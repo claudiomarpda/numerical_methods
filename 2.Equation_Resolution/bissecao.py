f313d278375b42af73f4b4f12bd681ff1bba732d
claudiomarpda <claudiomarpda@gmail.com>
2017-09-02 09:47:52 -0300

    Bissection and False Position methods updated with log system

--- 2.Equation_Resolution/bissecao.py
+++ 2.Equation_Resolution/bissecao.py
@@ -1,10 +1,12 @@
 """
-    Questao 01 da lista 02
-    Metodo da bissecao testado com tres equacoes diferentes para encontrar raizes aproximadas
-    de um intervalo inicial e uma tolerancia para f(x)
+Questao 01 da lista 02.
+Metodo da bissecao para encontrar raizes aproximadas de equacoes com um intervalo inicial e uma tolerancia para f(x).
 """
 
 import math
+
+# quando flag ativa, exibe informacoes durante o processo
+LOG = True
 
 '''
     f(x) = x^2 + log(x)
@@ -13,55 +15,68 @@ def equacao_um(x):
     return x ** 2 + math.log(x)
 
 '''
-    f(x) = ?
+    f(x) = x + ln(x)
 '''
 def equacao_dois(x):
-    # TODO: define and test this equation
+    return x + math.log(x)
 
 '''
     f(x) = ?
 '''
 def equacao_tres(x):
     # TODO: define and test this equation
+    return 0
 
 '''
-    @a: intervalo inferior
-    @b: intervalo superior
-    @tolerancia: objetivo a ser alcancado
-    @return uma raiz aproximada da equacao
+    @param a: intervalo inferior
+    @param b: intervalo superior
+    @param tolerancia: objetivo a ser alcancado
+    @return: uma raiz aproximada da equacao
 '''
 def bissecao(a, b, tolerancia):
+    global iteracoes
+    global f_de_x
+    
+    iteracoes = 0
+    f_de_x = 0
 
     # calcula f(a) e f(b)
-    resultado_a = equacao_um(a)
-    resultado_b = equacao_um(b)
+    f_de_a = equacao_um(a)
+    f_de_b = equacao_um(b)
 
     # verifica se existe raiz neste intervalo
-    if((resultado_a > 0 and resultado_b > 0) or (resultado_a < 0 and resultado_b < 0)):
+    if((f_de_a > 0 and f_de_b > 0) or (f_de_a < 0 and f_de_b < 0)):
         print 'Nao existe raiz no intervalo dado'
         return
     else:
-    	# procura por raiz
+        # procura por raiz
         while True:
-        	# ponto medio de [a, b]
+            iteracoes += 1
+            # formula da bissecao: ponto medio de [a, b]
             x = (a + b) / 2.0
             # f(x)
-            aproximacao_x = equacao_um(x)
-            # se f(x) = 0 ou
-            # se a tolerancia for alcancada,
+            f_de_x = equacao_um(x)
+
+            if(LOG):
+                print 'x = ' + str(x) + '; f(x) = ' + str(f_de_x)
+
+            # se f(x) = 0 ou se a tolerancia for alcancada,
             # x eh uma raiz da equacao
-            if(aproximacao_x == 0.0 or abs(aproximacao_x) < tolerancia):
+            if(f_de_x == 0.0 or abs(f_de_x) < tolerancia):
                 # x eh uma raiz aceitavel da equacao
                 return x
             # raiz nao encontrada
             else:
-                if(aproximacao_x > 0):
+                if(f_de_x > 0):
             		# novo intervalo: [a, x]
                     b = x
                 else:
                 	# novo intervalo: [x, b]
                     a = x
-
+                if(LOG):
+                    print 'Novo intervalo: [' + str(a) + ', ' + str(b) + ']'
 
 # Teste com o intervalo [0.5, 1] e tolerancia |f(x)| = 0.05
-print 'Raiz aproximada: ' + str(bissecao(0.5, 1.0, 0.05))
+print 'Intervalo [0.5, 1]; |f(x)| < 0.05'
+print 'Raiz aproximada x = ' + str(bissecao(0.5, 1.0, 0.05)) + '; f(x) = ' + str(f_de_x) + '; ' + str(iteracoes) + ' iteracoes'
+print
--- 2.Equation_Resolution/posicao_falsa.py
+++ 2.Equation_Resolution/posicao_falsa.py
@@ -1,4 +1,12 @@
+"""
+Lista 02. Questao 01.
+Metodo da posicao falsa para encontrar raizes aproximadas de equacoes com um intervalo inicial e uma tolerancia para f(x).
+"""
+
 import math
+
+# quando flag ativa, exibe informacoes durante o processo
+LOG = True
 
 '''
     f(x) = x^2 + log(x)
@@ -7,55 +15,68 @@ def equacao_um(x):
     return x ** 2 + math.log(x)
 
 '''
-	f(x) = ?
+	f(x) = x + ln(x)
 '''
 def equacao_dois(x):
-	# TODO: define and test this equation
+    return x + math.log(x)
 
 '''
 	f(x) = ?
 '''
 def equacao_tres(x):
-	# TODO: define and test this equation
-
+    # TODO: define and test this equation
+	return 0
 
 '''
-    @a: intervalo inferior
-    @b: intervalo superior
-    @tolerancia: objetivo a ser alcancado
-    @return uma raiz aproximada da equacao
+    @param a: intervalo inferior
+    @param b: intervalo superior
+    @param tolerancia: objetivo a ser alcancado
+    @return: uma raiz aproximada da equacao
 '''
 def posicao_falsa(a, b, tolerancia):
+    global iteracoes
+    global f_de_x
+
+    iteracoes = 0
+    f_de_x = 0
 
     # calcula f(a) e f(b)
-    resultado_a = equacao_um(a)
-    resultado_b = equacao_um(b)
+    f_de_a = equacao_um(a)
+    f_de_b = equacao_um(b)
 
     # verifica se existe raiz neste intervalo
-    if((resultado_a > 0 and resultado_b > 0) or (resultado_a < 0 and resultado_b < 0)):
+    if((f_de_a > 0 and f_de_b > 0) or (f_de_a < 0 and f_de_b < 0)):
         print 'Nao existe raiz no intervalo dado'
         return
     else:
     	# procura por raiz
         while True:
+            iteracoes += 1
         	# formula da posicao falsa
+            # (a * f(b) - b * f(a)) / (f(b) - f(a))
             x = (a * equacao_um(b) - b * equacao_um	(a)) / (equacao_um(b) - equacao_um(a))
             # f(x)
-            resultado_x = equacao_um(x)
-            # se f(x) = 0, x eh uma raiz da equacao ou
-            # se a tolerancia for alcancada
-            if(resultado_x == 0.0 or abs(resultado_x) < tolerancia):
+            f_de_x = equacao_um(x)
+            if(LOG):
+                print 'x = ' + str(x) + '; f(x) = ' + str(f_de_x)
+            # se f(x) = 0 ou se a tolerancia for alcancada,
+            # x eh uma raiz da equacao
+            if(f_de_x == 0.0 or abs(f_de_x) < tolerancia):
                 # x eh uma raiz da equacao
                 return x
             # raiz nao encontrada
             else:
-                if(resultado_x > 0):
+                if(f_de_x > 0):
             		# novo intervalo: [a, x]
                     b = x
                 else:
                 	# novo intervalo: [x, b]
                     a = x
+                if(LOG):
+                    print 'Novo intervalo: [' + str(a) + ', ' + str(b) + ']'
 
 
 # Teste com o intervalo [0.5, 1] e tolerancia |f(x)| = 0.05
-print 'Raiz aproximada: ' + str(posicao_falsa(0.5, 1.0, 0.05))
+print 'Intervalo [0.5, 1]; |f(x)| < 0.05'
+print 'Raiz aproximada x = ' + str(posicao_falsa(0.5, 1.0, 0.05)) + '; f(x) = ' + str(f_de_x) + '; ' + str(iteracoes) + ' iteracoes'
+print
