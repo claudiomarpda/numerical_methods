160a1d78a25cd9c762ce1600fc9ef9d2f21d1df8
claudiomarpda <claudiomarpda@gmail.com>
2017-11-06 19:00:18 -0300

    Utility functions separated in a single file

--- 4.Curve_Fitting/minimos_quadrados_discreto.py
+++ 4.Curve_Fitting/minimos_quadrados_discreto.py
@@ -1,8 +1,10 @@
 """
+    Caso discreto.
     Dada uma tabela com p-pontos(x1, y1), (x2, y2), ..., (xp, yp), implemente um programa
     que determina o polinomio de grau n, com n <= p, digamos pn(x) = a0 + a1x + ... an xn,
-    que melhor ajusta a tal tabela de pontos pelo metodo dos minimos quadrados.
-    Caso discreto.
+    que melhor ajusta a tal tabela de pontos pelo Metodo dos Minimos Quadrados.
+    Entrada: o numero p, a tabela de pontos e o grau n do polinomio ajuste.
+    Saida: os coeficientes a0, a1, ..., an do polinomio ajuste sobrepostos.
 """
 # Execute com Python3 com o seguinte comando no terminal:
 # -------------------------------------------------------
@@ -10,15 +12,13 @@
 # -------------------------------------------------------
 
 
-import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import style
+from util import *
 from eliminacao_gauss import f_eliminacao_gauss
 
 
 def minimos_quadrados_discreto(grau, n, x, y):
     """
-    Regressao polinomial pelo metodo dos Minimos Quadrados.
+    Regressao polinomial pelo Metodo dos Minimos Quadrados de forma generalizada.
     A partir de uma tabela de pontos, este metodo encontra um
     sistema de equacoes lineares do tipo Mx = v.
     Entao, resolvemos o sitema pelo metodo da Eliminacao de Gauss.
@@ -72,7 +72,6 @@ def minimos_quadrados_discreto(grau, n, x, y):
     # O vetor independente v de Mx = v
     v = np.empty([grau + 1])
     soma_y = 0
-    produto_x_y = 0
 
     # Percorre vetor de pontos y e calcula sua soma
     for i in range(0, n):
@@ -108,97 +107,6 @@ def minimos_quadrados_discreto(grau, n, x, y):
 
     # Obtem o valor dos coeficientes do polinomio
     return f_eliminacao_gauss(m, v)
-
-
-def exibe_polinomio(coef_poli):
-    """
-    Exibe no console de saida o polinomio com seus coeficientes de variaveis
-
-    :param coef_poli:
-    :return: vetor com os coeficientes do polinomio
-    """
-
-    print('Polinomio funcao ajuste', end='\n\n')
-    print('{0:.4f}'.format(coef_poli[0]), end='  ')
-    # Cada indice significa um coeficiente e um grau do polinomio
-    for i in range(1, len(coef_poli)):
-        # Coeficiente negativo
-        if coef_poli[i] < 0:
-            print(' -', end=' ')
-        # Coeficiente positivo
-        else:
-            print(' +', end=' ')
-
-        print('{0:.4f}'.format(coef_poli[i]), end=' (x^')
-        print(i, end=')')
-    print(end='\n\n')
-
-
-def exibe_figura(x, y, a):
-    """
-    Exibe uma figura com o diagrama de dispersao da tabela e o grafico do polinomio ajuste
-
-    :param x: vetor de pontos x da tabela
-    :param y: vetor de pontos y da tabela
-    :param a: vetor de pontos resultantes da funcao de ajuste
-    :return:
-    """
-
-    # TODO: Suavizar as curvas do grafico desenhado
-
-    style.use('ggplot')
-
-    # plota funcao ajuste
-    plt.plot(x, a, label='fit')
-    # plota diagrama de dispesao
-    plt.scatter(x, y)
-
-    plt.title('Ajuste de curvas')
-    plt.ylabel('Eixo Y')
-    plt.xlabel('Eixo X')
-    plt.show()
-
-
-def funcao_ajuste(x, coef):
-    """
-    Calcula f(x) para todos os pontos do vetor x da tabela
-
-    :param x: vetor de pontos x da tabela
-    :param coef: coeficientes do polinomio ajuste
-    :return: um vetor de pontos resultantes da funcao de ajuste
-    """
-
-    # n coeficientes
-    n = len(coef)
-
-    ajuste = np.empty([len(x)])
-
-    # Para cara ponto do vetor x
-    for i in range(0, len(x)):
-        # Para cada coeficiente
-        for j in range(0, n):
-            ajuste[i] = ajuste[i] + coef[j] * (x[i] ** j)
-
-    return ajuste
-
-
-def plot_poly(x_data, y_data, coeff, xlab='x', ylab='y'):
-    """
-        Plota funcao de um polinomio com curvas suaves
-    """
-    m = len(coeff)
-    x1 = min(x_data)
-    x2 = max(x_data)
-    dx = (x2 - x1) / 20.0
-    x = np.arange(x1, x2 + dx / 10.0, dx)
-    y = np.zeros((len(x))) * 1.0
-    for i in range(m):
-        y = y + coeff[i] * x ** i
-    plt.plot(x_data, y_data, 'o', x, y, '-')
-    plt.xlabel(xlab);
-    plt.ylabel(ylab)
-    plt.grid(True)
-    plt.show()
 
 
 def exibe_resultados(x, y, coef):
@@ -239,6 +147,7 @@ elif escolha == '2':
 
     coef_poli = minimos_quadrados_discreto(1, 5, x, y)
 
+    print('Polinomio ajuste', end='\n\n')
     exibe_resultados(x, y, coef_poli)
 
 elif escolha == '3':
@@ -248,6 +157,6 @@ elif escolha == '3':
     x = np.array([-1.0, -0.5, 0.0, 0.5, 1.0])
     y = np.array([2.0, 1.0, 0.5, 2.0, 1.5])
 
-    coef_poli = minimos_quadrados_discreto(1, 5, x, y)
+    coef_poli = minimos_quadrados_discreto(2, 5, x, y)
 
     exibe_resultados(x, y, coef_poli)
---
+++ 4.Curve_Fitting/util.py
@@ -0,0 +1,101 @@
+from matplotlib import pyplot as plt
+from matplotlib import style
+import numpy as np
+
+
+def exibe_polinomio(coef_poli):
+    """
+    Exibe no console de saida o polinomio com seus coeficientes de variaveis
+
+    :param coef_poli:
+    :return: vetor com os coeficientes do polinomio
+    """
+
+    print('Polinomio funcao ajuste', end='\n\n')
+    print('{0:.4f}'.format(coef_poli[0]), end='  ')
+    # Cada indice significa um coeficiente e um grau do polinomio
+    for i in range(1, len(coef_poli)):
+        # Coeficiente negativo
+        if coef_poli[i] < 0:
+            print(' -', end=' ')
+        # Coeficiente positivo
+        else:
+            print(' +', end=' ')
+
+        print('{0:.4f}'.format(coef_poli[i]), end=' (x^')
+        print(i, end=')')
+    print(end='\n\n')
+
+
+def exibe_figura(x, y, a):
+    """
+    Exibe uma figura com o diagrama de dispersao da tabela e o grafico do polinomio ajuste
+
+    :param x: vetor de pontos x da tabela
+    :param y: vetor de pontos y da tabela
+    :param a: vetor de pontos resultantes da funcao de ajuste
+    :return:
+    """
+
+    # TODO: Suavizar as curvas do grafico desenhado
+
+    style.use('ggplot')
+
+    # plota funcao ajuste
+    plt.plot(x, a, label='fit')
+    # plota diagrama de dispesao
+    plt.scatter(x, y)
+
+    plt.title('Ajuste de curvas')
+    plt.ylabel('Eixo Y')
+    plt.xlabel('Eixo X')
+    plt.show()
+
+
+def funcao_ajuste(x, coef):
+    """
+    Calcula f(x) para todos os pontos do vetor x da tabela
+
+    :param x: vetor de pontos x da tabela
+    :param coef: coeficientes do polinomio ajuste
+    :return: um vetor de pontos resultantes da funcao de ajuste
+    """
+
+    # n coeficientes
+    n = len(coef)
+
+    ajuste = np.empty([len(x)])
+
+    # Para cara ponto do vetor x
+    for i in range(0, len(x)):
+        # Para cada coeficiente
+        for j in range(0, n):
+            ajuste[i] = ajuste[i] + coef[j] * (x[i] ** j)
+
+    return ajuste
+
+
+def plot_poly(pontos_x, pontos_y, coef):
+    """
+        Plota funcao de um polinomio com curvas suaves
+    """
+
+    m = len(coef)
+    x1 = min(pontos_x)
+    x2 = max(pontos_x)
+    dx = (x2 - x1) / 20.0
+
+    x = np.arange(x1, x2 + dx / 10.0, dx)
+    y = np.zeros((len(x))) * 1.0
+
+    for i in range(m):
+        y = y + coef[i] * x ** i
+
+    xlab = 'Eixo x'
+    ylab = 'Eixo y'
+
+    plt.plot(pontos_x, pontos_y, 'o', x, y, '-')
+    plt.xlabel(xlab)
+    plt.ylabel(ylab)
+    plt.grid(True)
+    plt.show()
